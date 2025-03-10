48a97138e339d99dc4e8b29d27ef98384ee4fd99
João Carlos Matarazzo <100213241+JoaoCarlosMatarazzo@users.noreply.github.com>
2025-03-09 22:45:13 -0300

    07
    07

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 /* === Turns the menu hamburguer into X ===*/
 const menuHamburguer = document.querySelector('.menu-hamburguer');
-menuHamburguer.addEventListener('click',()=>{
-    toggleMenu();
-});
+menuHamburguer.addEventListener('click',()=>{toggleMenu();}
+);
 
 function toggleMenu(){
     const nav = document.querySelector('.nav-responsive');
